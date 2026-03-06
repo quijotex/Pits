03f974a5684b4d2eaac652b3ddc463bf8ccac73a
mariom <mariopresiga@gmail.com>
2026-03-05 22:59:33 -0500

    levantando ambiente

---
+++ backend/pist-api/src/test/java/com/pits/pist_api/PistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pits.pist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
